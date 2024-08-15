61c2d777f037019a609ade1b2dfeacd626536ecd
Ramya <202201409@daiict.ac.in>
2024-08-15 10:24:39 +0530

    minor typo in InputBox component solved

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-export const InputBox({label,placeholder}){
+export default function InputBox ({label,placeholder}){
     return <div>
         <div className="text-sm font-medium text-left py-2">
             {label}
